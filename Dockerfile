d3339951e253b0381e465e500d2a8bd37569b8d8
Brad Urani <bradurani@gmail.com>
2017-01-29 01:10:07 -0800

    Switch to async producer

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,21 @@
-FROM jruby:9.1-onbuild
+FROM jruby:9.1-jdk
+
 RUN apt-get update
 RUN apt-get install -y maven
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ADD Gemfile /usr/src/app/
+ADD Gemfile.lock /usr/src/app/
+RUN bundle install --system
 
 # Development Gems used in the Rakefile but not in the Gemfile
 RUN gem install nokogiri
 RUN gem install shoulda-context
 RUN cd $(bundle show telekinesis) && rake ext:build
 RUN cd $(bundle show telekinesis) && rake gem:build
+
+ADD . /usr/src/app
+
 CMD ["bundle","exec", "sidekiq", "-vr", "./sidekiq.rb"]
--- lib/workers/analytics_worker.rb
+++ lib/workers/analytics_worker.rb
@@ -1,10 +1,41 @@
 require 'sidekiq'
+require 'telekinesis'
 
 class AnalyticsWorker
   include Sidekiq::Worker
   sidekiq_options queue: :nodb, retry: true, backtrace: true
 
   def perform(*args)
-    puts "LookupWorker#perform fired with arguments #{args.map(&:inspect).join(', ')}"
+    require 'telekinesis'
+
+    producer = Telekinesis::Producer::AsyncProducer.create(
+      stream: 'teleport',
+      failure_handler: MyFailureHandler.new,
+      send_every_ms: 1500,
+      credentials: {
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    )
+    producer.put(1, args.join(','))
+  end
+
+end
+
+class MyFailureHandler
+  def on_record_failure(kv_pairs_and_errors)
+    puts kv_pairs_and_errors
+  end
+
+  def on_kinesis_error(err, items)
+    puts err, items
+  end
+
+  def on_kinesis_retry(err, items)
+    puts err, items
+  end
+
+  def on_kinesis_failure(err, items)
+    puts err, items
   end
 end
